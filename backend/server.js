e6d911f6728b651fa4093389eb41f08924e52850
Prasanna <sriprasanna.2k2@gmail.com>
2024-08-25 20:16:53 +0530

    Addition of post application api

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import app from "./app.js";
 import cloudinary from "cloudinary";
 
 cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
+  cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_CLIENT_API,
   api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
 });
